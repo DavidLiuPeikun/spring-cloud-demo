f3d58f779985db14f4977991fa087153a9d8e842
BaronLiu <wutongnazi@163.com>
2018-04-01 23:32:28 +0800

    init2

--- reverse-proxy/src/main/java/cn/demo/reverseProxy/Application.java
+++ reverse-proxy/src/main/java/cn/demo/reverseProxy/Application.java
@@ -16,6 +16,7 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 @EnableEurekaClient
 public class Application {
     public static void main(String[] args) {
+       // SpringApplication.run(Application.class, args);
         SpringApplication.run(Application.class, args);
     }
 }
--- service1/src/main/java/cn/demo/service1/Application.java
+++ service1/src/main/java/cn/demo/service1/Application.java
@@ -21,6 +21,6 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        //SpringApplication.run(Application.class, args);
     }
 }
